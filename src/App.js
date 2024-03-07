5b867a62b11bc00e0e407f629701dc1e8deb71e1
Zhenya <57942027+Zhenya72@users.noreply.github.com>
2024-03-07 21:15:51 +0200

    Add files via upload
    create project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Туточки буде мій портфорліо а покищо відосік ЧІПІ-ЧАПИ Ги ГИ
+
+      <div className="video-container">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/watch?v=g7tzDP3oI2Y&list=LL&index=1&t=290s"
+          title="YouTube Video"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default App;
