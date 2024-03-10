abbeef2173c796d06e0df29d076c51565111d421
Zhenya <57942027+Zhenya72@users.noreply.github.com>
2024-03-10 19:54:21 +0200

    Add files via upload

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -20,7 +20,7 @@ function Footer() {
                     <a className="link" href="https://t.me/Zhenya_72"><Telegram className='icon'/></a>
                   </li>
                   <li>
-                    <a className="link" href="https://t.me/Zhenya_72"><Linkedin className='icon'/></a>
+                    <a className="link" href="https://www.linkedin.com/in/%D1%94%D0%B2%D0%B3%D0%B5%D0%BD%D1%96%D0%B9-%D0%B1%D0%B0%D1%80%D0%B0%D0%BD%D0%BE%D0%B2%D1%81%D1%8C%D0%BA%D0%B8%D0%B9-0a85262b9"><Linkedin className='icon'/></a>
                   </li>
                 </ul>
               </address>
--- src/Sections/Contact/Contact.js
+++ src/Sections/Contact/Contact.js
@@ -30,9 +30,9 @@ function Contact() {
                     </a>
                   </li>
                   <li>
-                    <a className="link" href="https://t.me/Zhenya_72">
+                    <a className="link" href="https://www.linkedin.com/in/%D1%94%D0%B2%D0%B3%D0%B5%D0%BD%D1%96%D0%B9-%D0%B1%D0%B0%D1%80%D0%B0%D0%BD%D0%BE%D0%B2%D1%81%D1%8C%D0%BA%D0%B8%D0%B9-0a85262b9">
                       <Linkedin className='icon'/>
-                      @Zhenya_72
+                      Linkedin
                     </a>
                   </li>
                 </ul>
